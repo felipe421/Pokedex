9d54c693ec2f628bc3b3cc1992dbaae08274d683
Brenno Alves <brennoao@outlook.com>
2023-10-19 21:11:18 -0300

    homeScreen_Final

--- pokedex/pages/HomeScreen.js
+++ pokedex/pages/HomeScreen.js
@@ -8,7 +8,7 @@ export default function HomeScreen(props) {
     const [pokemons, setPokemons] = useState([]);
 
     useEffect(() => {
-        PokedexApi.get('/pokemon?limit=10').then(response => {
+        PokedexApi.get('/pokemon?limit=2000').then(response => {
             let pokemonList = response.data.results
             pokemonList.map((item, i) => {
                 item.id = i + 1
@@ -27,21 +27,18 @@ export default function HomeScreen(props) {
     //     console.log(endPoints)
     // }
 
-
-
-
-
     return (
         <View style={styles.Container}>
             <FlatList
                 // style={styles.list}
-                
+                numColumns={3}
                 data={pokemons}
                 keyExtractor={item => String(item.id)}
+                columnWrapperStyle={{gap: 10, width: '100%', justifyContent: 'space-between'}}
                 renderItem={({ item, index }) => (
                     // <></>
-                    <Card mode='outlined' style={{ marginBottom: 10 }} onPress={() => { props.navigation.navigate('TelaInformacao', (index + 1)) }}>
-                        <Card.Cover style={{ resizeMode: 'stretch' }} source={{ uri: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/' + (index + 1) + '.png' }} />
+                    <Card mode='outlined' style={{marginBottom: 10, flex: 1, borderRadius: 5 }} onPress={() => { props.navigation.navigate('TelaInformacao', (index + 1)) }}>
+                         <Card.Cover style={{ width: '100%', height: 100, resizeMode: 'stretch', borderRadius: 5, padding: 4 }}  source={{ uri: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/' + (index + 1) + '.png' }} />
                         <Card.Title title={item.name} />
                         {/* <Text>Tipo: {getPokemonType(item.id)}</Text> */}
                     </Card>
