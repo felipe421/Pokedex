1e25cb3d891fdcaf7cda79efb044908f48999c49
iteduardojr <it.eduardojr@gamil.com>
2023-10-19 21:03:03 -0300

    testando

--- pokedex/pages/HomeScreen.js
+++ pokedex/pages/HomeScreen.js
@@ -1,67 +1,64 @@
-import { FlatList, StyleSheet, View } from 'react-native'
-import React, { useEffect, useState } from 'react'
-import PokedexApi from '../services/PokedexApi'
-import { Card, Text } from 'react-native-paper'
-import { ScrollView } from 'react-native-gesture-handler'
+import { FlatList, StyleSheet, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios'; // Importe o axios para fazer solicitações à API
+import PokedexApi from '../services/PokedexApi';
+import { Card, Text } from 'react-native-paper';
 
 export default function HomeScreen(props) {
-    const [pokemons, setPokemons] = useState([])
+    const [pokemons, setPokemons] = useState([]);
 
     useEffect(() => {
-        PokedexApi.get('/pokemon?limit=10').then(response => {
-            let pokemonList = response.data.results
-            pokemonList.map((item, i) => {
-                item.id = i + 1
-                return item
-            })
-            setPokemons(pokemonList)
-            console.log(pokemonList)
-        })
-    }, [])
+        PokedexApi.get('/pokemon?limit=10').then(async (response) => {
+            const pokemonList = response.data.results;
+            const updatedPokemonList = await Promise.all(
+                pokemonList.map(async (item, i) => {
+                    // Busque as informações de tipo de cada Pokémon
+                    const pokemonData = await axios.get(item.url);
+                    const types = pokemonData.data.types.map((type) => type.type.name);
+                    item.types = types;
+                    item.id = i + 1;
+                    return item;
+                })
+            );
+            setPokemons(updatedPokemonList);
+        });
+    }, []);
 
-    // const Test = () => {
-    //     var endPoints = []
-    //     for (var i = 1; i < 2000; i++) {
-    //         endPoints.push(PokedexApi + '/pokemon/' + i + '/')
-    //     }
-    //     console.log(endPoints)
-    // }
-
-
-
-
+    async function getPokemonType(id) {
+        try {
+          const response = await PokedexApi.get('/type/' + id + '/');
+          const tipo = response.data
+          console.log(tipo.name)
+          return tipo.name
+       
+        } catch (error) {
+          console.error(error);
+          return null; // retorna null em caso de erro
+        }
+      }
 
     return (
         <View style={styles.Container}>
             <FlatList
                 style={styles.list}
                 data={pokemons}
-                keyExtractor={item => String(item.id)}
+                keyExtractor={(item) => String(item.id)}
                 renderItem={({ item, index }) => (
-                    // <></>
-                    <Card mode='outlined' style={{ marginBottom: 10 }} onPress={() => { props.navigation.navigate('TelaInformacao', (index + 1)) }}>
-                        <Card.Cover style={{ resizeMode: 'stretch' }} source={{ uri: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/' + (index + 1) + '.png' }} />
+                    <Card mode='outlined' style={{ marginBottom: 10 }} onPress={() => { props.navigation.navigate('TelaInformacao', item.id) }}>
+                        <Card.Cover style={{ resizeMode: 'stretch' }} source={{ uri: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/' + item.id + '.png' }} />
                         <Card.Title title={item.name} />
-                        {/* <Card.Title title={index} /> */}
+                        {/* <Text>Tipo: {getPokemonType(item.id)}</Text> */}
                     </Card>
                 )}
-
             />
-            {/* {pokemons.map((item, i) => (
-                <Card mode='outlined' style={{ marginBottom: 10 }} onPress={() => { props.navigation.navigate('TelaInformacao', (i + 1)) }}>
-                    <Card.Cover style={{ resizeMode: 'stretch' }} source={{ uri: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/' + (i + 1) + '.png' }} />
-                    <Card.Title title={item.name} />
-                </Card>
-            ))} */}
         </View>
-    )
+    );
 }
 
 const styles = StyleSheet.create({
     Container: {
         maxWidth: '100%',
         padding: 10,
-        backgroundColor: '#FFFFFF'
-
-    }
-})
+        backgroundColor: '#FFFFFF',
+    },
+});
